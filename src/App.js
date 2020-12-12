b1d657253d9b7a01ffdc9a0c8f9fb8c43cf3896f
Humaira-K <71674591+Humaira-K@users.noreply.github.com>
2020-12-11 21:22:02 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>Weather App</h1> 
-     
+
+
+
+
+     <footer id="github-linkedin-link">
+      <p>
+        <a
+          href="https://github.com/Humaira-K/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-Source Code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/humairakhan/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Humaira Khan
+        </a>
+      </p>
+    </footer>
+    </div>
     </div>
   );
 }
